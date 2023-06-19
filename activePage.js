4cdcd45e2c5e0f48a50cee9509355a286fed20e0
JJGlas <jglas1193@gmail.com>
2023-06-19 12:56:32 -0700

    Add files via upload

--- activePage.js
+++ activePage.js
@@ -1,24 +1,14 @@
-/*underline active link YT: https://youtu.be/JkuiKeNS2mg looks at current page of whatever page we are viewing
+/*underline active link YT: https://youtu.be/JkuiKeNS2mg looks at current page of whatever page we are viewing*/
 const activePage = window.location.pathname;
 const navLinks = document.querySelectorAll('nav a').forEach(link => {
   if(link.href.includes(`${activePage}`)){
     link.classList.add('active');
     console.log(link);
   }
+  /*else(${activePage} == link(1);)*/
+  /*if(link.href == null){
+    ${activePage} == link(1);
+  }*/ 
 })
-*/
-/*underline active link YT: https://youtu.be/JkuiKeNS2mg looks at current page of whatever page we are viewing*/
-const navLinkEls = document.querySelectorAll('.navbar__links');
-const windowPathname = window.location.pathname;
-
-navLinkEls.forEach(navLinkEl => {
-  const navLinkPathname = new URL(navLinkEl.href).pathname;
-
-  if (windowPathname === navLinkPathname){
-    navLinkEl.classList.add('active');
-  }
-});
-
-
 
 
