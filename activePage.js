dc92fdb48615b1d00e3f420b82850ed6fd50ac34
JJGlas <jglas1193@gmail.com>
2023-06-21 17:56:11 -0700

    Add files via upload

--- activePage.js
+++ activePage.js
@@ -1,14 +1,33 @@
-/*underline active link YT: https://youtu.be/JkuiKeNS2mg looks at current page of whatever page we are viewing*/
+/*underline active link YT: https://youtu.be/JkuiKeNS2mg looks at current page of whatever page we are viewing
 const activePage = window.location.pathname;
 const navLinks = document.querySelectorAll('nav a').forEach(link => {
   if(link.href.includes(`${activePage}`)){
     link.classList.add('active');
     console.log(link);
   }
-  /*else(${activePage} == link(1);)*/
-  /*if(link.href == null){
-    ${activePage} == link(1);
-  }*/ 
 })
+*/
+/*underline active link YT: https://youtu.be/JkuiKeNS2mg looks at current page of whatever page we are viewing. may want to replace .navbar__links with nav a*/
+const navLinkEls = document.querySelectorAll(".navbar__links");
+const windowPathname = window.location.pathname;
+
+navLinkEls.forEach(navLinkEl => {
+  const navLinkPathname = new URL(navLinkEl.href).pathname;
+
+  if (windowPathname === navLinkPathname){
+    navLinkEl.classList.add('active');
+  }
+});
+
+/*other method on YT: https://youtu.be/mP2LnkOqLHk
+let links = document.querySelectorAll(".navbar__links");
+let bodyId = document.querySelector("body").id;
+ 
+for(let link of links){
+    if(link.dataset.active == bodyId){
+        link.classList.add("active");
+    }
+}
+*/
 
 
